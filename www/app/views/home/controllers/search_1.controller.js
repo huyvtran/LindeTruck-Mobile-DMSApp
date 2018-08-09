dc6fb48c3b8ee725284abe667b6b583c96376731
BoomGit <battlejacker@gmail.com>
2018-08-09 15:30:49 +0800

    修改搜索页功能

--- www/app/views/home/controllers/search_1.controller.js
+++ www/app/views/home/controllers/search_1.controller.js
@@ -2,29 +2,45 @@
     'use strict';
 
     angular.module('oinio.Search_1controllers', [])
+        .filter('filterSearchChild',function () {
+            return function (text) {
+                    return text.indexOf($scope.searchChildText);
+            };
+        })
         .controller('Search_1Controller', function ($scope, $rootScope, $filter, $state, $stateParams, ConnectionMonitor,
-            LocalCacheService,$ionicHistory,AppUtilService,AccountService,$ionicPopup) {
+            LocalCacheService,$ionicHistory,AppUtilService,AccountService) {
             var vm = this;
 
 
 
             $scope.toRepair4 = function (){
                 console.log("toRepair4");
-                 $state.go('app.customDetail');
-
+                $state.go('app.customDetail');
              };
 
             $scope.$on("$ionicView.enter",function () {
+                console.log("init some quantities");
                 AppUtilService.showLoading();
+                $scope.searchText ="";
+                $scope.searchChildText ="";
+                $scope.button_clicked=false;
                 $scope.isShow=true;
+                $scope.isBigTypeShow = true;
                 $scope.items =  loadingData();
             });
 
+            $scope.clearParentType =function(){
+                $scope.searchText ="";
+            };
+            $scope.clearChildType = function(){
+                $scope.searchChildText ="";
+            };
 
-
-            $scope.showChildInfoList = function () {
+            $scope.showChildInfoList = function (keyWord) {
                 console.log("showChildInfoList");
+                $scope.button_clicked=true;
                 $scope.searchText = "";
+                $scope.isBigTypeShow=false;
                 $scope.isShow=false;
                 
                  //调用接口获取结果
@@ -53,6 +69,7 @@
 
             var loadingData = function () {
                 AppUtilService.hideLoading();
+                console.log("get type data");
                 return ["查询订单", "查询车档", "查询发货单"];
             };
         });
