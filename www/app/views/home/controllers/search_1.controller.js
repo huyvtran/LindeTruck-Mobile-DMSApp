c4c854532cc447bf9a6df72f4f9a3f155e800119
guowei <147321000@qq.com>
2018-08-09 15:04:44 +0800

    数据装载

--- www/app/views/home/controllers/search_1.controller.js
+++ www/app/views/home/controllers/search_1.controller.js
@@ -3,21 +3,21 @@
 
     angular.module('oinio.Search_1controllers', [])
         .controller('Search_1Controller', function ($scope, $rootScope, $filter, $state, $stateParams, ConnectionMonitor,
-            LocalCacheService,$ionicHistory,AppUtilService,AccountService) {
+            LocalCacheService,$ionicHistory,AppUtilService,AccountService,$ionicPopup) {
             var vm = this;
 
 
 
             $scope.toRepair4 = function (){
                 console.log("toRepair4");
-                $state.go('app.customDetail');
+                 $state.go('app.customDetail');
+
              };
 
             $scope.$on("$ionicView.enter",function () {
                 AppUtilService.showLoading();
                 $scope.isShow=true;
                 $scope.items =  loadingData();
-                AccountService.searchAccounts("buwen");
             });
 
 
@@ -26,9 +26,28 @@
                 console.log("showChildInfoList");
                 $scope.searchText = "";
                 $scope.isShow=false;
-                $scope.contentItems = ["100130011","100130012","100130013"];
-                //console.log("AccountService",AccountService.searchAccounts("查询发货单 001"));
-                //$state.go('app.customDetail');
+                
+                 //调用接口获取结果
+                 AccountService.searchAccounts("buwen").then(function (response) {
+                    console.log("AccountServicegw",response[0]);
+                    console.log("AccountServicegw",response[1]);
+                    console.log("AccountServicegw",response);
+                    let accountsName = [];
+                    if (response.length > 0) {
+                        for (let index = 0; index < response.length; index++) {
+                            accountsName.push(response[index].Name);
+                        }
+                        $scope.contentItems = accountsName;
+                    }
+                    else {
+                        $ionicPopup.alert({
+                            title: "结果",
+                            template: "没有数据"
+                        });
+                    }
+                }, function (error) {
+                    $log.error('AccountService.searchAccounts Error ' + error);
+                });    
             };
 
 
