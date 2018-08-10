9a3ff3d191e3f47d7068751212a8ffbb883b4306
BoomGit <battlejacker@gmail.com>
2018-08-10 12:05:58 +0800

    修改打电话功能

--- www/app/views/home/controllers/customDetail.controller.js
+++ www/app/views/home/controllers/customDetail.controller.js
@@ -2,50 +2,76 @@
     'use strict';
 
     angular.module('oinio.CustomDetailController', [])
-        .controller('CustomDetailController', function ($scope, $ionicPopup,$rootScope, $filter, $state ,$stateParams, $ionicHistory, AccountService) {
-
-            //调用接口获取结果
-            console.log("$stateParams.SendPassId", $stateParams.SendPassId);
-            AccountService.getAccount($stateParams.SendPassId).then(function (account) {
-                console.log("getAccount", account);
-
-                if (account != null) {
-                    document.getElementById("accountName").textContent = account.Name;
-                    
-                    document.getElementById("accountAddress").textContent = account.Address__c;
-                    document.getElementById("accountSAP").textContent = account.SAP_Number__c;
-
-                    console.log("getAccountgw", account.Name);
-                }
-                else {
-                    $ionicPopup.alert({
-                        title: "结果",
-                        template: "没有数据"
-                    });
-                }
-            }, function (error) {
-                $log.error('AccountService.searchAccounts Error ' + error);
+        .controller('CustomDetailController', function ($scope, $ionicPopup,$rootScope, $filter, $log,$state ,$stateParams, $ionicHistory, AccountService
+        ,AppUtilService) {
+            $scope.$on("$ionicView.beforeEnter",function () {
+                $scope.accountName="";
+                $scope.accountAddress="";
+                $scope.accountSAP="";
+                $rootScope.accountId="";
             });
-            
-            //调用接口获取结果
-            AccountService.getContacts("001p000000Qx9m2AAB").then(function (contacts) {
-                console.log("getContacts", contacts);
-                let accountsName = [];
-                if (contacts.length > 0) {
-                    for (let index = 0; index < contacts.length; index++) {
-                        accountsName.push(contacts[index].Name);
+
+            $scope.$on("$ionicView.enter",function () {
+                //AppUtilService.showLoading();
+                getBaseInfo();
+                getContactInfo();
+            });
+
+            $scope.$on("$ionicView.leave",function () {
+
+            });
+
+            //获取基础信息
+            var getBaseInfo = function(){
+                //调用接口获取结果
+                console.log("$stateParams.SendPassId", $stateParams.SendPassId);
+                AccountService.getAccount($stateParams.SendPassId).then(function (account) {
+                    console.log("getAccount", account);
+
+                    if (account != null) {
+                        $rootScope.accountId = account.Id;
+                        $scope.accountName=account.Name;
+                        $scope.accountAddress=account.Address__c;
+                        $scope.accountSAP=account.SAP_Number__c;
+                        console.log("getAccount", account.Name);
                     }
-                    $scope.contentItems = accountsName;
-                }
-                else {
-                    $ionicPopup.alert({
-                        title: "结果",
-                        template: "没有数据"
-                    });
-                }
-            }, function (error) {
-                $log.error('AccountService.searchAccounts Error ' + error);
-            });
+                    else {
+                        $ionicPopup.alert({
+                            title: "结果",
+                            template: "没有数据"
+                        });
+                    }
+                }, function (error) {
+                    $log.error('AccountService.searchAccounts Error ' + error);
+                });
+            };
+
+            //获取联系人信息
+            var getContactInfo =function(){
+                //调用接口获取结果 $rootScope.accountId
+                AccountService.getContacts("001p000000Qx9m2AAB").then(function (contacts) {
+                    //{"Id":"003p000000MnltsAAB","Name":"吴工","MobilePhone":null}
+                    console.log("getContacts", contacts);
+                    //let accountsNames = [];
+                    if (contacts.length > 0) {
+                        // for (let index = 0; index < contacts.length; index++) {
+                        //     accountsNames.push(contacts[index].Name);
+                        // }
+                        //$scope.contentItems = accountsNames;
+                        $scope.contentItems = contacts;
+                    }
+                    else {
+                        $ionicPopup.alert({
+                            title: "结果",
+                            template: "联系人信息没有数据"
+                        });
+                    }
+                }, function (error) {
+                    $log.error('AccountService.searchAccounts Error ' + error);
+                });
+            };
+
+
             $scope.toDisplayBaseInfo = function () {
                 if (document.getElementById("div_baseInfo").style.display == "none") {
                     document.getElementById("div_baseInfo").style.display = "";//隐藏
