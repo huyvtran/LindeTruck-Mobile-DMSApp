98e64368d6c9ab3ac0ea14bb91d5d935442efca3
guowei <147321000@qq.com>
2018-09-17 17:06:29 +0800

    1.报价页样式

--- www/app/views/home/controllers/newOffer.controller.js
+++ www/app/views/home/controllers/newOffer.controller.js
@@ -1,8 +1,21 @@
 angular.module('oinio.NewOfferController', [])
     .controller('NewOfferController', function ($scope, $ionicPopup, $stateParams, HomeService, $state, $rootScope) {
         var toDisplayDelCarBool = false;
+        var toDisplaySearchBool = false;
+
         $scope.goBack = function () {
             window.history.go(-1);
+        };
+        $scope.goSearch = function () {
+            var searchGroupDiv = document.getElementById("searchGroupDiv");
+            if (toDisplaySearchBool) {
+                toDisplaySearchBool = false;
+                searchGroupDiv.className = 'search_Group to_Hide';
+            } else {
+                toDisplaySearchBool = true;
+                searchGroupDiv.className = 'search_Group';
+
+            }
         };
         $scope.toDisplayDelCarView = function () {
             var div = document.getElementById("addDelCarView");/*w和h都要用这部分，故单独定义一个div*/
@@ -23,40 +36,42 @@ angular.module('oinio.NewOfferController', [])
         };
 
         $scope.toAddTableView = function () {
-            // var subtable = document.getElementById('subject_tab'); 
-
 
             //添加一个列
-            count = 1;
-            // var txtTDLastIndex = findObj("txtTDLastIndex", document);
-            // var columnID = parseInt(txtTDLastIndex.value);
-            var div = document.getElementById("tabExample");
-            div.style.height = (count*80)+'px';
-            count++;
-            var columnID = "1";
+            // count = 1;
+            // var div = document.getElementById("tabExample");
+            // div.style.height = (count*80)+'px';
+            // count++;
+            // var columnID = "1";
 
-            var tab = document.getElementById("tabExample");
-            var rowLength = tab.rows.length;
-            var columnLength = tab.rows[1].cells.length;
-            console.log("columnLength:", columnLength);
-            for (var i = 0; i < rowLength; i++) {
-                if (i == 0) {
-                    var oTd = tab.rows[0].insertCell(columnLength);
-                    oTd.innerHTML = "<tr> <td style='border: 1px solid #979797 background: #eee9e9;padding: 0 1%;font-size: 0.8rem; height: 40px;width: 80px;'>  <div> 车体号1 </div> </td></tr> ";
-                } else if (i == 1) {//第一列:序号
-                    var oTd = tab.rows[1].insertCell(columnLength);
-                    oTd.innerHTML = "<tr> <td style='border: 1px solid #979797'>  <input class='ad_Input'/> </td></tr> ";
-                } else if (i > 1) {
-                    var oTd = tab.rows[i].insertCell(columnLength);
-                    oTd.id = "column" + columnID;
-                    oTd.innerHTML = "<tr> <td style='border: 1px solid #979797'>  <div> 车体号1 </div> </td></tr> ";
-                }
-            }
-
-
-
-
-
+            // var tab = document.getElementById("tabExample");
+            // var rowLength = tab.rows.length;
+            // var columnLength = tab.rows[1].cells.length;
+            // console.log("columnLength:", columnLength);
+            // for (var i = 0; i < rowLength; i++) {
+            //     if (i == 0) {
+            //         var oTd = tab.rows[0].insertCell(columnLength);
+            //         oTd.innerHTML = "<tr> <td style='border: 1px solid #979797 background: #eee9e9;padding: 0 1%;font-size: 0.8rem; height: 40px;width: 80px;'>  <div> 车体号1 </div> </td></tr> ";
+            //     } else if (i == 1) {//第一列:序号
+            //         var oTd = tab.rows[1].insertCell(columnLength);
+            //         oTd.innerHTML = "<tr> <td style='border: 1px solid #979797'>  <input class='ad_Input'/> </td></tr> ";
+            //     } else if (i > 1) {
+            //         var oTd = tab.rows[i].insertCell(columnLength);
+            //         oTd.id = "column" + columnID;
+            //         oTd.innerHTML = "<tr> <td style='border: 1px solid #979797'>  <div> 车体号1 </div> </td></tr> ";
+            //     }
+            // }
+            var tabExample = document.getElementById("tabExample");
+            var tabExampleDiv = document.getElementById("tabExampleDiv");
+            var tabAdd = document.getElementById("tabExampleAdd");
+            // 获得h1里的内容
+            var com = tabExample.innerHTML;
+            // 克隆节点
+            var con = tabExample.cloneNode();
+            // 将内容写到克隆的节点内
+            con.innerHTML = com;
+            // console.log(con);
+            tabExampleDiv.insertBefore(con,tabAdd);
 
         }
         $scope.submitOrder = function () {
