6476652ea78cd64af056bc5c4677b3e35e192102
Buwen Guo <buwen.guo@capgemini.com>
2018-09-19 17:08:57 +0800

    报价模块添加车辆信息接口

--- www/app/views/home/services/serviceQuote.service.js
+++ www/app/views/home/services/serviceQuote.service.js
@@ -28,7 +28,7 @@
                 let sql =  "select {Account:_soup}\
                          from {Account}\
                          where {Account:Name} like '%"+keyword+"%'\
-                             or {Account:SAP_Number__c} like '%"+keyword+"%'";
+                             or {Account:Customer_Number__c} like '%"+keyword+"%'";
                 let querySpec = navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
                 navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
                     let accounts = [];
@@ -37,7 +37,8 @@
                             accounts.push({
                                 Id: entry[0].Id,
                                 Name: entry[0].Name,
-                                SAP_Number__c: entry[0].SAP_Number__c,
+                                Customer_Number__c: entry[0].Customer_Number__c,
+                                Office_Address__c: entry[0].Office_Address__c,
                                 _soupEntryId: entry[0]._soupEntryId
                             });
                         });
@@ -78,6 +79,8 @@
                                 Salesman__c: entry[0].Salesman__c,
                                 Salesman_formula__c: entry[0].Salesman_formula__c,
                                 Sale_Group_Code__c: entry[0].Sale_Group_Code__c,
+                                Customer_Number__c: entry[0].Customer_Number__c,
+                                Office_Address__c: entry[0].Office_Address__c,
                                 _soupEntryId: entry[0]._soupEntryId
                             };
                         });
@@ -122,6 +125,167 @@
                 console.log('getContacts::', deferred.promise);
                 return deferred.promise;
             };
+
+
+            /**
+             * @func  searchTrucks
+             * @desc  search (fuzzy) Truck_Fleet__c list by name
+             * @param {String} keyword - part of Truck_Fleet__c name
+             * @returns {Promise} [Truck_Fleet__c] - Truck_Fleet__c list, including Id, Name etc.
+             */
+
+            this.searchTrucks = function(keyword){
+                console.log('searchTrucks.keyword:%s', keyword);
+                let deferred = $q.defer();
+
+                let sql =  "select {Truck_Fleet__c:_soup}\
+                         from {Truck_Fleet__c}\
+                         where {Truck_Fleet__c:Name} like '%"+keyword+"%'";
+
+                let querySpec = navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
+                navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
+                    let trucks = [];
+                    if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
+                        angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
+                            trucks.push({
+                                Id: entry[0].Id,
+                                Name: entry[0].Name,
+                                Family__c: entry[0].Family__c,
+                                Model__c: entry[0].Model__c,
+                                Ship_To_CS__c: entry[0].Ship_To_CS__c,
+                                Maintenance_Key__c: entry[0].Maintenance_Key__c,
+                                _soupEntryId: entry[0]._soupEntryId
+                            });
+                        });
+                    }
+                    deferred.resolve(trucks);
+                }, function (err) {
+                    $log.error(err);
+                    console.error(err);
+                    deferred.reject(err);
+                });
+                console.log('searchTrucks::', deferred.promise);
+                return deferred.promise;
+            };
+
+
+
+            /**
+             * @func  getMaintenanceLevelsAndDescriptions
+             * @desc  get all Maintenance Levels and Maintenance Descriptions for certain Maintenance_Key__c
+             * @param {String} keyword - Truck_Fleet__c.Maintenance_Key__c
+             * @returns {Promise} Object.levels(Maintenance Levels[] - String list)
+             *             Object.descriptions(Maintenance Descriptions[] - String list)
+             */
+
+            this.getMaintenanceLevelsAndDescriptions = function(keyword){
+                console.log('getMaintenanceLevels.keyword:%s', keyword);
+                let deferred = $q.defer();
+
+                let sql =  "select {Maintenance_Key_Level_Parts__c:_soup}\
+                         from {Maintenance_Key_Level_Parts__c}\
+                         where {Maintenance_Key_Level_Parts__c:Name} like '%"+keyword+"%'";
+
+                let querySpec = navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
+                navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
+                    let result = new Object();
+                    let mlevels = [];
+                    let mDescs = [];
+                    if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
+                        angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
+                            let bool_existMlevel = false;
+                            angular.forEach(mlevels, function (mitem) {
+                                if (mitem == entry[0].Maintenance_Level__c) {
+                                    bool_existMlevel = true;
+                                }
+                            });
+                            if (!bool_existMlevel && entry[0].Maintenance_Level__c != null && entry[0].Maintenance_Level__c != '') {
+                                mlevels.push(entry[0].Maintenance_Level__c);
+                            }
+
+                            let bool_existMDescs = false;
+                            angular.forEach(mDescs, function (mDitem) {
+                                if (mDitem == entry[0].Maintenance_Description__c) {
+                                    bool_existMDescs = true;
+                                }
+                            });
+                            if (!bool_existMDescs && entry[0].Maintenance_Description__c != null && entry[0].Maintenance_Description__c != '') {
+                                mDescs.push(entry[0].Maintenance_Description__c);
+                            }
+                        });
+                        result.levels = mlevels;
+                        result.descriptions = mDescs;
+                    }
+                    deferred.resolve(result);
+                }, function (err) {
+                    $log.error(err);
+                    console.error(err);
+                    deferred.reject(err);
+                });
+                console.log('getMaintenanceLevels::', deferred.promise);
+                return deferred.promise;
+            };
+
+
+            /**
+             * @func  getDefaultWorkHour
+             * @desc  get default work hour for certain Maintenance_Key__c,Maintenance_Level__c and Maintenance_Description__c.
+             * @param {String} keyword - selected Truck_Fleet__c.Maintenance_Key__c
+             *      {String} level - selected Maintenance_Level__c in page
+             *      {String} desp - selected Maintenance_Description__c in page
+             * @returns {Promise} Decimal[] - available work hours list
+             */
+
+            this.getDefaultWorkHour = function(keyword,level,desp){
+                console.log('getDefaultWorkHour.keyword:', keyword);
+                console.log('getDefaultWorkHour.level:', level);
+                console.log('getDefaultWorkHour.desp:', desp);
+                let deferred = $q.defer();
+
+                if((keyword == null || keyword == '') && (level == null || level == '') && (desp == null || desp == '')){
+                    deferred.resolve([]);
+                }
+
+                let sql =  "select {Maintenance_Key_Level_Parts__c:_soup}\
+                         from {Maintenance_Key_Level_Parts__c}\
+                         where {Maintenance_Key_Level_Parts__c:Id} != null";
+
+                if(keyword != null && keyword != '') {
+                    sql = sql + " and {Maintenance_Key_Level_Parts__c:Name} like '%" + keyword + "%'";
+                }
+                if(level != null && level != '') {
+                    sql = sql + " and {Maintenance_Key_Level_Parts__c:Maintenance_Level__c} like '%" + level + "%'";
+                }
+                if(desp != null && desp != '') {
+                    sql = sql + " and {Maintenance_Key_Level_Parts__c:Maintenance_Description__c} like '%" + desp + "%'";
+                }
+
+                let querySpec = navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
+                navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
+                    let results = [];
+                    if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
+                        angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
+                            let bool_existHour = false;
+                            angular.forEach(results, function (mitem) {
+                                if (mitem == entry[0].Work_Hour__c) {
+                                    bool_existHour = true;
+                                }
+                            });
+                            if (!bool_existHour && entry[0].Work_Hour__c != null && entry[0].Work_Hour__c != '') {
+                                results.push(entry[0].Work_Hour__c);
+                            }
+                        });
+                    }
+                    deferred.resolve(results);
+                }, function (err) {
+                    $log.error(err);
+                    console.error(err);
+                    deferred.reject(err);
+                });
+                console.log('getDefaultWorkHour::', deferred.promise);
+                return deferred.promise;
+            };
+
 
 
             /**
