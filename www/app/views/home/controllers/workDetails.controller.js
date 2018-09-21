4942b0773915c1b7b5793392da60bb28f4ccd80a
BoomGit <battlejacker@gmail.com>
2018-09-21 10:39:16 +0800

    修改工单详情页界面

--- www/app/views/home/controllers/workDetails.controller.js
+++ www/app/views/home/controllers/workDetails.controller.js
@@ -1,8 +1,9 @@
 angular.module('oinio.workDetailsControllers', [])
-    .controller('workDetailsController', function ($scope, $rootScope, $filter, $state, $log,$stateParams, ConnectionMonitor,
+    .controller('workDetailsController', function ($scope, $rootScope, $filter, $state, $log,$ionicPopup,$stateParams, ConnectionMonitor,
                                                    LocalCacheService,HomeService) {
 
         var vm = this,
+            destinationType,
             oCurrentUser = LocalCacheService.get('currentUser') || {};
 
         vm.isOnline = null;
@@ -17,6 +18,7 @@ angular.module('oinio.workDetailsControllers', [])
             LocalCacheService.set('previousStateParamsForSCReady', $stateParams);
             $scope.TruckId ="";
             $scope.HasTruckNum = 0;
+            $scope.imgUris =["././img/images/will_add_img.png"];
         });
         $scope.$on('$ionicView.enter', function () {
             vm.isOnline = ConnectionMonitor.isOnline();
@@ -43,17 +45,17 @@ angular.module('oinio.workDetailsControllers', [])
                         $scope.busyerType ='';
                     }
                     console.log('resp::',response);
-                    HomeService.getTrucksForParentOrderSid(userInfoId).then(function (res) {
-                        var str= "";
-                        angular.forEach(res,function (item) {
-                            str += item.Name+";\n";
-                        });
-                        $scope.HasTruckNum = res !=null ? res.length: 0;
-                        $scope.TruckId = str;
-                    },function (error) {
-                        $log.error('Error ' + error);
-                        $scope.TruckId ="";
-                    });
+                    // HomeService.getTrucksForParentOrderSid(userInfoId).then(function (res) {
+                    //     var str= "";
+                    //     angular.forEach(res,function (item) {
+                    //         str += item.Name+";\n";
+                    //     });
+                    //     $scope.HasTruckNum = res !=null ? res.length: 0;
+                    //     $scope.TruckId = str;
+                    // },function (error) {
+                    //     $log.error('Error ' + error);
+                    //     $scope.TruckId ="";
+                    // });
                 }else{
                     $scope.busyerName ="";
                     $scope.busyerType="";
@@ -67,13 +69,57 @@ angular.module('oinio.workDetailsControllers', [])
             });
         });
 
+        /**
+         * 获取图片
+         * 1.拍照
+         * 2.从相册取
+         */
+        $scope.getPhoto = function(element){
+            $ionicPopup.show({
+                title: '选择图片',
+                buttons: [
+                    {   text: '拍照' ,
+                        onTap: function(e) {
+                            navigator.camera.getPicture(function onPhotoDataSuccess(imgUri) {
+                                $scope.imgUris.push(imgUri);
+                                }, function onFail(error) {
+                                    console.log(error)
+                                    $log.error(error);
+                                }, {
+                                quality: 50,
+                                destinationType: navigator.camera.DestinationType.FILE_URI }
+                            );
+
+                        }
+                    },
+                    {
+                        text: '相册',
+                        onTap: function(e) {
+                            navigator.camera.getPicture(function onPhotoURISuccess(imgUri) {
+                                    $scope.imgUris.push(imgUri);
+                                    console.log(imgUri);
+                                    $log.error(error);
+                                },
+                                function onFail(error){
+                                    console.log(error);
+                                },
+                                {
+                                    quality: 50,
+                                    destinationType: navigator.camera.DestinationType.FILE_URI,
+                                    sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY });
+                        }
+                    },
+                ]
+            })
+        };
+
         $scope.toDetailInfo = function(){
             if (document.getElementById("detailContent").style.display == "none") {
                 document.getElementById("detailContent").style.display = "";//显示
                 document.getElementById("detailImg").className = "OpenClose_Btn arrow_Down_White";
             } else {
                 document.getElementById("detailContent").style.display = "none";//隐藏
-                document.getElementById("detailImg").className = "OpenClose_Btn arrow_Down_White";
+                document.getElementById("detailImg").className = "OpenClose_Btn arrow_Left_White";
 
             }
         };
@@ -83,7 +129,7 @@ angular.module('oinio.workDetailsControllers', [])
                 document.getElementById("workImg").className = "OpenClose_Btn arrow_Down_White";
             } else {
                 document.getElementById("workContent").style.display = "none";//隐藏
-                document.getElementById("workImg").className = "OpenClose_Btn arrow_Down_White";
+                document.getElementById("workImg").className = "OpenClose_Btn arrow_Left_White";
 
             }
         };
@@ -93,7 +139,7 @@ angular.module('oinio.workDetailsControllers', [])
                 document.getElementById("partImg").className = "OpenClose_Btn arrow_Down_White";
             } else {
                 document.getElementById("partContent").style.display = "none";//隐藏
-                document.getElementById("partImg").className = "OpenClose_Btn arrow_Down_White";
+                document.getElementById("partImg").className = "OpenClose_Btn arrow_Left_White";
 
             }
         };
@@ -103,7 +149,7 @@ angular.module('oinio.workDetailsControllers', [])
                 document.getElementById("serviceImg").className ="OpenClose_Btn arrow_Down_White";
             } else {
                 document.getElementById("serviceContent").style.display = "none";//隐藏
-                document.getElementById("serviceImg").className = "OpenClose_Btn arrow_Down_White";
+                document.getElementById("serviceImg").className = "OpenClose_Btn arrow_Left_White";
 
             }
         };
