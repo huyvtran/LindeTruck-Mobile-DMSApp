6ee0248b8bb4552eda0228d19df98b13946fb120
guowei <147321000@qq.com>
2018-10-09 16:17:35 +0800

    1.报价页面 审批接口

--- www/app/views/home/controllers/newOfferFittings.controller.js
+++ www/app/views/home/controllers/newOfferFittings.controller.js
@@ -1,5 +1,5 @@
 angular.module('oinio.NewOfferFittingsController', [])
-    .controller('NewOfferFittingsController', function ($scope, $http, $ionicPopup, $stateParams, HomeService, $state, AppUtilService ,$rootScope, SQuoteService, ForceClientService) {
+    .controller('NewOfferFittingsController', function ($scope, $http, $ionicPopup, $stateParams, HomeService, $state, AppUtilService, $rootScope, SQuoteService, ForceClientService) {
 
         // var forceClient = ForceClientService.getForceClient();
         // console.log("forceClient", forceClient);
@@ -17,6 +17,7 @@ angular.module('oinio.NewOfferFittingsController', [])
         $scope.paramUrl1 = "/Parts/7990110000/" + $stateParams.SendSoupEntryId;
         $scope.paramUrl2 = "/Parts/7990110003/" + $stateParams.SendSoupEntryId;
         $scope.paramSaveUrl = "/ServiceQuoteOverview?";
+        $scope.paramApprovalsUrl = "/v38.0/process/approvals";
 
         $scope.get = function () {
             ForceClientService.getForceClient().apexrest($scope.paramUrl1, 'GET', {}, null, function (response) {
@@ -40,7 +41,7 @@ angular.module('oinio.NewOfferFittingsController', [])
                 console.log("error:", error);
             });
         }
-       
+
         $scope.$on('$ionicView.enter', function () {
             console.log("NewOfferFittingsController");
             $scope.get();
@@ -68,7 +69,7 @@ angular.module('oinio.NewOfferFittingsController', [])
 
         $scope.toDelSVView = function (index) {
             $scope.serviceFeeList.baoremove(index);
-            
+
         };
 
         $scope.sum = function (obj) {
@@ -96,14 +97,6 @@ angular.module('oinio.NewOfferFittingsController', [])
             oneLabourOriginals2["Quantity__c"] = $scope.manMadeNo2;
             oneLabourOriginals2["Discount__c"] = $scope.discountPrice2;
             $scope.quoteLabourOriginalsList.push(oneLabourOriginals2);
-        };
-        //保存
-        $scope.toSaveServiceQuoteOverview = function (payload) {
-            AppUtilService.showLoading();
-            $scope.addLabourOriginalsList();
-            var serviceQuoteOverview = {};
-            serviceQuoteOverview["Ship_to__c"] = $stateParams.SendSoupEntryId;
-
             // var serviceQuotes = [];//车辆
             // var quoteLabourOriginals = [];//劳务费
             for (let index = 0; index < $stateParams.SendAllUser.length; index++) {
@@ -113,35 +106,7 @@ angular.module('oinio.NewOfferFittingsController', [])
                 delete element.Id;
                 delete element.Name;
             }
-            $scope.toGetTogether();//组织劳务费数据
-            var payload =$scope.paramSaveUrl+ "serviceQuoteOverview="+JSON.stringify(serviceQuoteOverview)+"&serviceQuotes="+ JSON.stringify($stateParams.SendAllUser)+ "&quoteLabourOriginals="+JSON.stringify($scope.quoteLabourOriginalsList);
-            console.log("payload",payload);
 
-
-            ForceClientService.getForceClient().apexrest(payload, 'POST', {}, null, function (response) {
-                AppUtilService.hideLoading();
-                console.log("POST_success:", response);
-                var ionPop = $ionicPopup.alert({
-                    title: "保存成功"
-                });
-                ionPop.then(function (res) {
-                    window.history.back(-1);
-                    window.history.back(-1);
-                });
-            }, function (error) {
-                console.log("POST_error:", error);
-                AppUtilService.hideLoading();
-                var ionPop = $ionicPopup.alert({
-                    title: "保存失败"
-                });
-            });
-        };
-        //提交审核
-        $scope.toSubmitCheck = function(){
-        }
-
-        //组织劳务费添加
-        $scope.toGetTogether = function(){
             var sv_InputForListPrice = [];//单价
             var sv_InputForListNo = [];//数量
             var sv_InputForListDiscount = [];//折扣
@@ -165,16 +130,102 @@ angular.module('oinio.NewOfferFittingsController', [])
 
             for (let index = 0; index < $scope.serviceFeeList.length; index++) {
                 var oneLabourOriginals3 = {};
-            oneLabourOriginals3["Name"] = $scope.serviceFeeList[index];
-            oneLabourOriginals3["Gross_Amount__c"] = sv_InputForListPrice[index];
-            oneLabourOriginals3["Quantity__c"] = sv_InputForListNo[index];
-            oneLabourOriginals3["Discount__c"] = sv_InputForListDiscount[index];
-            oneLabourOriginals3["Net_Amount__c"] = sv_InputForListSpecial[index];
+                oneLabourOriginals3["Name"] = $scope.serviceFeeList[index];
+                oneLabourOriginals3["Gross_Amount__c"] = sv_InputForListPrice[index];
+                oneLabourOriginals3["Quantity__c"] = sv_InputForListNo[index];
+                oneLabourOriginals3["Discount__c"] = sv_InputForListDiscount[index];
+                oneLabourOriginals3["Net_Amount__c"] = sv_InputForListSpecial[index];
 
-            $scope.quoteLabourOriginalsList.push(oneLabourOriginals3);
-                
+                $scope.quoteLabourOriginalsList.push(oneLabourOriginals3);
+
             }
-            
-        }
+
+        };
+        //保存
+        $scope.toSaveServiceQuoteOverview = function (payload) {
+            AppUtilService.showLoading();
+            $scope.addLabourOriginalsList();//组织劳务费数据
+            var serviceQuoteOverview = {};
+            serviceQuoteOverview["Ship_to__c"] = $stateParams.SendSoupEntryId;
+
+            var payload = $scope.paramSaveUrl + "serviceQuoteOverview=" + JSON.stringify(serviceQuoteOverview) + "&serviceQuotes=" + JSON.stringify($stateParams.SendAllUser) + "&quoteLabourOriginals=" + JSON.stringify($scope.quoteLabourOriginalsList);
+            console.log("payload", payload);
+
+            ForceClientService.getForceClient().apexrest(payload, 'POST', {}, null, function (response) {
+                AppUtilService.hideLoading();
+                console.log("POST_success:", response);
+                var ionPop = $ionicPopup.alert({
+                    title: "保存成功"
+                });
+                ionPop.then(function (res) {
+                    window.history.back(-1);
+                    window.history.back(-1);
+                });
+            }, function (error) {
+                console.log("POST_error:", error);
+                AppUtilService.hideLoading();
+                var ionPop = $ionicPopup.alert({
+                    title: "保存失败"
+                });
+            });
+        };
+
+        //提交审核
+        $scope.toSubmitCheckFunction = function (sendId) {
+            var listForrequests = [];
+            var payload1 = {};
+            payload1["actionType"] = "Submit";
+            payload1["contextId"] = sendId;
+            payload1["comments"] = "comments";
+            listForrequests.push(payload1);
+            var requests = {};
+            requests["requests"] = listForrequests;
+
+            // path, callback, error, method, payload, headerParams
+            ForceClientService.getForceClient().ajax($scope.paramApprovalsUrl, function (response) {
+                AppUtilService.hideLoading();
+                console.log("toSubmitCheckFunction_success:", response);
+                var ionPop = $ionicPopup.alert({
+                    title: "提交成功"
+                });
+                ionPop.then(function (res) {
+                    window.history.back(-1);
+                    window.history.back(-1);
+                });
+            }, function (error) {
+                console.log("toSubmitCheckFunction_error:", error);
+                AppUtilService.hideLoading();
+                var ionPop = $ionicPopup.alert({
+                    title: "提交失败"
+                });
+            }, 'POST', JSON.stringify(requests), null);
+            console.log("toSubmitCheckFunction_payload", requests);
+
+        };
+
+        $scope.toSubmitCheck = function () {
+            AppUtilService.showLoading();
+            $scope.addLabourOriginalsList();//组织劳务费数据
+            var serviceQuoteOverview = {};
+            serviceQuoteOverview["Ship_to__c"] = $stateParams.SendSoupEntryId;
+
+            var payload = $scope.paramSaveUrl + "serviceQuoteOverview=" + JSON.stringify(serviceQuoteOverview) + "&serviceQuotes=" + JSON.stringify($stateParams.SendAllUser) + "&quoteLabourOriginals=" + JSON.stringify($scope.quoteLabourOriginalsList);
+            console.log("payload", payload);
+
+            ForceClientService.getForceClient().apexrest(payload, 'POST', {}, null, function (response) {
+                console.log("POST_success:", response);
+                $scope.toSubmitCheckFunction(response.Id);//提交审核接口
+
+            }, function (error) {
+                console.log("POST_error:", error);
+                AppUtilService.hideLoading();
+                var ionPop = $ionicPopup.alert({
+                    title: "保存失败"
+                });
+            });
+
+        };
+
+
     });
 
