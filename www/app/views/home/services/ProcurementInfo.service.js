1e723aa278873f2b942df00de3824bb77d88d083
jinliang0604@foxmail.com <jinliang0604@foxmail.com>
2018-11-08 13:43:23 +0800

    采购申请-搜索供应商接口 V1.1

--- www/app/views/home/services/ProcurementInfo.service.js
+++ www/app/views/home/services/ProcurementInfo.service.js
@@ -10,7 +10,7 @@
      */
     angular
         .module('oinio.services')
-        .service('ProcurementInfoService', function($q, $filter, LocalDataService, ConnectionMonitor, IonicLoadingService, LocalSyncService) {
+        .service('ProcurementInfoService', function($q, $filter, LocalDataService, ConnectionMonitor, IonicLoadingService, LocalSyncService, SMARTSTORE_COMMON_SETTING) {
 
             let service = this;
 
@@ -23,6 +23,7 @@
                     var sql = "select {Supplier_Information__c:_soup}\
                          from {Supplier_Information__c}\
                          where {Supplier_Information__c:Name} like '%"+codeOrName+"%'\
+                         or {Supplier_Information__c:Supplier_Code__c} like '%"+codeOrName+"%'
                          order by {Supplier_Information__c:_soupEntryId} desc limit 1";
                     console.log('current query sql:::', sql);
 
@@ -31,12 +32,12 @@
                         var result = new Object();
                         if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
                             angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
-                                result['Province__c'] = entry[0].Mobile_Offline_Name__c;
-                                result['City__c'] = entry[0].Work_Order_Type__c;
-                                result['Address__c'] = entry[0].Description__c;
-                                result['Post_Code__c'] = entry[0].Service_Suggestion__c;
-                                result['Telephone__c'] = entry[0].Description__c;
-                                result['FAX__c'] = entry[0].Service_Suggestion__c;
+                                result['Province__c'] = entry[0].Province__c;
+                                result['City__c'] = entry[0].City__c;
+                                result['Address__c'] = entry[0].Address__c;
+                                result['Post_Code__c'] = entry[0].Post_Code__c;
+                                result['Telephone__c'] = entry[0].Telephone__c;
+                                result['FAX__c'] = entry[0].FAX__c;
                                 result._soupEntryId = entry[0]._soupEntryId;
                             });
                         }
