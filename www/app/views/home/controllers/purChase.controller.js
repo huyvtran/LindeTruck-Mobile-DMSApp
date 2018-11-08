e4700ca4814aabff3cad901eda45c8d0b4d05f43
BoomGit <battlejacker@gmail.com>
2018-11-08 11:19:11 +0800

    添加供应商查询页面以及物料查询页面

--- www/app/views/home/controllers/purChase.controller.js
+++ www/app/views/home/controllers/purChase.controller.js
@@ -1,41 +1,140 @@
-angular.module('oinio.PurChaseController', [])
-    .controller('PurChaseController', function ($scope, $rootScope, $filter, $state,$log, $stateParams, ConnectionMonitor,
-                                             LocalCacheService) {
+(function () {
+    'use strict';
 
-        var vm = this,
-            oCurrentUser = LocalCacheService.get('currentUser') || {};
 
-        vm.isOnline = null;
+    angular.module('oinio.PurChaseController', [])
+        .controller('PurChaseController', function ($scope, $rootScope, $filter, $state,$log,$ionicPopup,$stateParams, ConnectionMonitor,
+                                                    LocalCacheService) {
 
-        /**
-         * @func    $scope.$on('$ionicView.beforeEnter')
-         * @desc
-         */
-        $scope.$on('$ionicView.beforeEnter', function () {
-            LocalCacheService.set('previousStateForSCReady', $state.current.name);
-            LocalCacheService.set('previousStateParamsForSCReady', $stateParams);
+            var vm = this,
+                oCurrentUser = LocalCacheService.get('currentUser') || {};
+            $scope.recordTypes = [];
+            vm.isOnline = null;
 
+            /**
+             * @func    $scope.$on('$ionicView.beforeEnter')
+             * @desc
+             */
+            $scope.$on('$ionicView.beforeEnter', function () {
+                LocalCacheService.set('previousStateForSCReady', $state.current.name);
+                LocalCacheService.set('previousStateParamsForSCReady', $stateParams);
+
+                var calendar = new lCalendar();
+                calendar.init({
+                    'trigger': '#planDate',
+                    'type': 'date'
+                });
+            });
+            $scope.$on('$ionicView.enter', function () {
+                // check if device is online/offline
+                vm.isOnline = ConnectionMonitor.isOnline();
+                if (oCurrentUser) {
+                    vm.username = oCurrentUser.Name;
+                }
+
+                $scope.recordTypes.push({value:"Service Parts Procurement"});
+                $scope.recordTypes.push({value:"Service Support Procurement"});
+
+
+
+
+
+
+
+
+            });
+
+            $scope.searchForBusiness =function(){
+                if (document.getElementById("serachBusinessContent").style.display == "none") {
+                    document.getElementById("serachBusinessContent").style.display = "block";//显示
+                    document.getElementById("busyAllContent").style.display = "none";//隐藏
+                } else {
+                    document.getElementById("serachBusinessContent").style.display = "none";//隐藏
+                    document.getElementById("busyAllContent").style.display = "block";//显示
+                }
+            };
+
+            $scope.showBusinessInfo = function(){
+                if (document.getElementById("businessBox").style.display == "none") {
+                    document.getElementById("businessBox").style.display = "block";//显示
+                    document.getElementById("businessInfoImg").className = "arrow_Down_White";
+                } else {
+                    document.getElementById("businessBox").style.display = "none";//隐藏
+                    document.getElementById("businessInfoImg").className = "arrow_Down_White";
+                }
+            };
+
+
+            $scope.showBaseInfo = function(){
+                if (document.getElementById("baseBugInfo").style.display == "none") {
+                    document.getElementById("baseBugInfo").style.display = "block";//显示
+                    document.getElementById("baseInfoImg").className = "OpenClose_Btn arrow_Down_White";
+                } else {
+                    document.getElementById("baseBugInfo").style.display = "none";//隐藏
+                    document.getElementById("baseInfoImg").className = "OpenClose_Btn arrow_Down_White";
+                }
+            };
+
+
+            $scope.showMaterialInfo = function(){
+                if (document.getElementById("materialContent").style.display == "none") {
+                    document.getElementById("materialContent").style.display = "block";//显示
+                    document.getElementById("materialImg").className = "OpenClose_Btn arrow_Down_White";
+                } else {
+                    document.getElementById("materialContent").style.display = "none";//隐藏
+                    document.getElementById("materialImg").className = "OpenClose_Btn arrow_Down_White";
+
+                }
+
+            };
+
+
+
+            $scope.addOrDelMaterial = function(){
+                if (document.getElementById("materialAddBox").className == "btn_modify_Div to_Hide") {
+                    document.getElementById("materialAddBox").className = "btn_modify_Div to_Open";//显示
+                } else {
+                    document.getElementById("materialAddBox").className = "btn_modify_Div to_Hide";//隐藏
+                }
+            };
+
+
+            $scope.showContentHideBusiness = function(){
+                document.getElementById("busyAllContent").style.display == "block";
+                document.getElementById("serachBusinessContent").style.display == "none";
+            };
+
+
+            $scope.showContentHideMaterial = function(){
+                document.getElementById("busyAllContent").style.display == "block";
+                document.getElementById("serachMaterialContent").style.display == "none";
+            };
+
+
+
+
+
+
+            $scope.goBack =function () {
+                window.history.back();
+            };
+
+
+            $scope.goSavePurChase =function () {
+                var planDate = $("#planDate").val();
+
+                if (!planDate){
+                    $ionicPopup.alert({
+                        title: "请选择计划日期"
+                    });
+                    return;
+                }
+
+
+                $state.go('app.home');
+            };
 
         });
-        $scope.$on('$ionicView.enter', function () {
-            // check if device is online/offline
-            vm.isOnline = ConnectionMonitor.isOnline();
-            if (oCurrentUser) {
-                vm.username = oCurrentUser.Name;
-            }
 
-        });
-
-        $scope.goBack =function () {
-            window.history.back();
-        };
-
-        $scope.goSavePurChase =function () {
-            $state.go('app.home');
-        };
-
-
-
-
-    });
+})();
 
