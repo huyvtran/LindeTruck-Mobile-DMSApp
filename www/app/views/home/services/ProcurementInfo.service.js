980ce7f1b50ce94edf63f1cc79b575059ef3df41
jinliang0604@foxmail.com <jinliang0604@foxmail.com>
2018-11-08 16:04:43 +0800

    采购申请-搜索供应商接口 V2.0

--- www/app/views/home/services/ProcurementInfo.service.js
+++ www/app/views/home/services/ProcurementInfo.service.js
@@ -54,35 +54,34 @@
                 return deferred.promise;
             }
 
-            /*
             this.queryServiceMaterial = function (materialName) {
                 var deferred = $q.defer();
 
-                if(codeOrName == '' || codeOrName == null){
+                if(materialName == '' || materialName == null){
                     deferred.reject('Search string can not be null!');
                 }else{
-                    var sql = "select {Supplier_Information__c:_soup}\
-                         from {Supplier_Information__c}\
-                         where {Supplier_Information__c:Name} like '%"+codeOrName+"%'\
-                         or {Supplier_Information__c:Supplier_Code__c} like '%"+codeOrName+"%'
-                    order by {Supplier_Information__c:_soupEntryId} desc limit 1";
+                    var sql = "select {parts__c:_soup}\
+                         from {parts__c}\
+                         where {parts__c:Name} like '%"+materialName+"%'\
+                    order by {parts__c:_soupEntryId}";
                     console.log('current query sql:::', sql);
 
-                    var querySuInfo= navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
-                    navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
-                        var result = new Object();
+                    var querySmInfo= navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
+                    navigator.smartstore.runSmartQuery(querySmInfo, function (cursor) {
+                        var results = [];
                         if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
                             angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
-                                result['Province__c'] = entry[0].Province__c;
-                                result['City__c'] = entry[0].City__c;
-                                result['Address__c'] = entry[0].Address__c;
-                                result['Post_Code__c'] = entry[0].Post_Code__c;
-                                result['Telephone__c'] = entry[0].Telephone__c;
-                                result['FAX__c'] = entry[0].FAX__c;
+                                var result = new Object();
+                                result['Name'] = entry[0].Name;
+                                result['parts_description__c'] = entry[0].parts_description__c;
+                                result['Factory__c'] = entry[0].Factory__c;
+                                result['Cost_Price__c'] = entry[0].Cost_Price__c;
                                 result._soupEntryId = entry[0]._soupEntryId;
+                                results.push(result);
                             });
                         }
-                        deferred.resolve(result);
+                        console.log('Service Material results:::', results);
+                        deferred.resolve(results);
                     }, function (err) {
                         console.error(err);
                         deferred.reject(err);
@@ -90,7 +89,6 @@
                 }
                 return deferred.promise;
             }
-            */
 
             this.synchronize = function () {
                 var deferred = $q.defer();
