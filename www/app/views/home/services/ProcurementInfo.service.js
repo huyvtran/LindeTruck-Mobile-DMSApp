4e410b8e3d817e7cac7f20eaf505d5268dd7ad37
jinliang0604@foxmail.com <jinliang0604@foxmail.com>
2018-11-08 11:30:37 +0800

    采购申请-搜索供应商接口 V1.0

---
+++ www/app/views/home/services/ProcurementInfo.service.js
@@ -0,0 +1,109 @@
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name oinio.services:ProcurementInfoService
+     *
+     * @description
+     */
+    angular
+        .module('oinio.services')
+        .service('ProcurementInfoService', function($q, $filter, LocalDataService, ConnectionMonitor, IonicLoadingService, LocalSyncService) {
+
+            let service = this;
+
+            this.querySupplierInformation = function (codeOrName) {
+                var deferred = $q.defer();
+
+                if(codeOrName == '' || codeOrName == null){
+                    deferred.reject('Search string can not be null!');
+                }else{
+                    var sql = "select {Supplier_Information__c:_soup}\
+                         from {Supplier_Information__c}\
+                         where {Supplier_Information__c:Name} like '%"+codeOrName+"%'\
+                         order by {Supplier_Information__c:_soupEntryId} desc limit 1";
+                    console.log('current query sql:::', sql);
+
+                    var querySuInfo= navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
+                    navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
+                        var result = new Object();
+                        if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
+                            angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
+                                result['Province__c'] = entry[0].Mobile_Offline_Name__c;
+                                result['City__c'] = entry[0].Work_Order_Type__c;
+                                result['Address__c'] = entry[0].Description__c;
+                                result['Post_Code__c'] = entry[0].Service_Suggestion__c;
+                                result['Telephone__c'] = entry[0].Description__c;
+                                result['FAX__c'] = entry[0].Service_Suggestion__c;
+                                result._soupEntryId = entry[0]._soupEntryId;
+                            });
+                        }
+                        deferred.resolve(result);
+                    }, function (err) {
+                        console.error(err);
+                        deferred.reject(err);
+                    });
+                }
+                return deferred.promise;
+            }
+
+            this.synchronize = function () {
+                var deferred = $q.defer();
+
+                if (ConnectionMonitor.isOnline()) {
+                    IonicLoadingService.show($filter('translate')('cl.sync.lb_synchronizing'));
+                    LocalSyncService.syncUpObjectByAll().then(function () {
+                        IonicLoadingService.hide();
+                        deferred.resolve();
+                    });
+                } else {
+                    deferred.resolve();
+                }
+
+                return deferred.promise;
+            };
+
+            this.cloneObj = function (obj) {
+                var newObj = new Object();
+                if(obj.RecordTypeId != null && obj.RecordTypeId != '') {
+                    newObj['RecordTypeId'] = obj.RecordTypeId;
+                    newObj['RecordTypeId_sid'] = obj.RecordTypeId_sid;
+                    newObj['RecordTypeId_type'] = 'RecordType';
+                }
+                return newObj;
+            };
+
+
+        });
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
