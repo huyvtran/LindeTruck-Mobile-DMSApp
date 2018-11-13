048dc4a0a772d3b0135ad7a91e74635be210cf5c
BoomGit <battlejacker@gmail.com>
2018-11-13 15:59:00 +0800

    修改工单详情页1.保存时取消对来电是由进行非空判断2.取消来电是由不可编辑的限制

--- www/app/views/home/controllers/workDetails.controller.js
+++ www/app/views/home/controllers/workDetails.controller.js
@@ -121,11 +121,11 @@ angular.module('oinio.workDetailsControllers', [])
             $scope.getRefundList();
             //************************* */
 
-            if (allowEdit) {
-                $("#call_str").removeAttr("disabled");
-            } else {
-                $("#call_str").attr("disabled", "disabled");
-            }
+            // if (allowEdit) {
+            //     $("#call_str").removeAttr("disabled");
+            // } else {
+            //     $("#call_str").attr("disabled", "disabled");
+            // }
 
             var numArr1 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
             var numArr2 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60'];
@@ -476,12 +476,12 @@ angular.module('oinio.workDetailsControllers', [])
 
         $scope.doPrint = function ($event) {
             var callStr = $("#call_str").val().trim();
-            if (callStr == "" || callStr == null) {
-                $ionicPopup.alert({
-                    title: "请填写来电是由!"
-                });
-                return;
-            }
+            // if (callStr == "" || callStr == null) {
+            //     $ionicPopup.alert({
+            //         title: "请填写来电是由!"
+            //     });
+            //     return;
+            // }
 
             if (arriveTime == null) {
                 $ionicPopup.alert({
@@ -597,12 +597,12 @@ angular.module('oinio.workDetailsControllers', [])
         $scope.goSave = function () {
 
             var callStr = $("#call_str").val().trim();
-            if (callStr == "" || callStr == null) {
-                $ionicPopup.alert({
-                    title: "请填写来电是由!"
-                });
-                return;
-            }
+            // if (callStr == "" || callStr == null) {
+            //     $ionicPopup.alert({
+            //         title: "请填写来电是由!"
+            //     });
+            //     return;
+            // }
 
             if (arriveTime == null || finishTime == null || startTime == null || leaveTime == null) {
                 var ionPop = $ionicPopup.alert({
