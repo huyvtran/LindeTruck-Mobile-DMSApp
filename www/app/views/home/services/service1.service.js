68278bb02ff18f457cbe280af1e4e35731317442
Buwen Guo <buwen.guo@capgemini.com>
2019-01-21 15:27:16 +0800

    新建工单页双模接口

--- www/app/views/home/services/service1.service.js
+++ www/app/views/home/services/service1.service.js
@@ -20,10 +20,11 @@
              */
             this.getUserObjectById = function (str_userId,isOnline) {
                 if(isOnline){
-                    let url = service.buildURL('querySobject','select id from user where id = ""');
+                    let sql = "select id,Name from user where id = '" + str_userId + "' limit 50";
+                    let url = service.buildURL('querySobject',sql);
                     let requestMethod = 'GET';
                     return service.sendRest(url,requestMethod);
-                }else{
+                } else {
                     let deferred = $q.defer();
 
                     let sql =  "select {User:_soup}\
@@ -50,6 +51,144 @@
 
 
 
+
+
+            /**
+             * replace function of HomeService.searchAccounts
+             */
+            this.searchAccounts = function(keyword,isOnline){
+                if(isOnline){
+                    let sql = "select id,Name,Customer_Number__c from Account where Name like '%" + keyword +
+                                "%' or Customer_Number__c like '%" + keyword + "%' limit 50";
+                    let url = service.buildURL('querySobjects',sql);
+                    let requestMethod = 'GET';
+                    return service.sendRest(url,requestMethod);
+                } else {
+                    let deferred = $q.defer();
+
+                    let sql = "select {Account:_soup}\
+                         from {Account}\
+                         where {Account:Name} like '%" + keyword + "%'\
+                             or {Account:Customer_Number__c} like '%" + keyword + "%'";
+                    let querySpec = navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
+                    navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
+                        let accounts = [];
+                        if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
+                            angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
+                                accounts.push({
+                                    Id: entry[0].Id,
+                                    Name: entry[0].Name,
+                                    Customer_Number__c: entry[0].Customer_Number__c,
+                                    _soupEntryId: entry[0]._soupEntryId
+                                });
+                            });
+                        }
+                        deferred.resolve(accounts);
+                    }, function (err) {
+                        $log.error(err);
+                        console.error(err);
+                        deferred.reject(err);
+                    });
+                    console.log('searchAccounts::', deferred.promise);
+                    return deferred.promise;
+                }
+            };
+
+
+            /**
+             * replace function of HomeService.getLatest3ServiceOrders
+             */
+            this.getLatest3ServiceOrders = function(acctId,isOnline){
+                if(isOnline){
+                    let sql = "select Id,Name,Account_Ship_to__c,Service_Order_Type__c,Service_Order_Owner__c,Status__c,"+
+                            "Plan_Date__c,Truck_Serial_Number__c,Description__c from Service_Order_Overview__c " +
+                            " where Account_Ship_to__c ='" + acctId + "' order by Id desc limit 3";
+                    let url = service.buildURL('querySobjects',sql);
+                    let requestMethod = 'GET';
+                    return service.sendRest(url,requestMethod);
+                } else {
+                    console.log('getLatest3ServiceOrders.keyword:%s', acctId);
+                    let deferred = $q.defer();
+
+                    let sql = "select {Service_Order_Overview__c:_soup}\
+                         from {Service_Order_Overview__c}\
+                         where {Service_Order_Overview__c:Account_Ship_to__c_sid} = '" + acctId + "' \
+                         order by {Service_Order_Overview__c:_soupEntryId} desc limit 3";
+                    let querySpec = navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
+                    navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
+                        let orders = [];
+                        if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
+                            angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
+                                let accId = entry[0].Account_Ship_to__c;
+                                orders.push({
+                                    Id: entry[0].Id,
+                                    Name: entry[0].Name,
+                                    Account_Ship_to__c: accId,
+                                    Service_Order_Type__c: entry[0].Service_Order_Type__c,
+                                    Service_Order_Owner__c: entry[0].Service_Order_Owner__c,
+                                    Status__c: entry[0].Status__c,
+                                    Plan_Date__c: entry[0].Plan_Date__c,
+                                    Truck_Serial_Number__c: entry[0].Truck_Serial_Number__c,
+                                    Description__c: entry[0].Description__c,
+                                    _soupEntryId: entry[0]._soupEntryId
+                                });
+                            });
+                        }
+                        deferred.resolve(orders);
+                    }, function (err) {
+                        $log.error(err);
+                        console.error(err);
+                        deferred.reject(err);
+                    });
+                    console.log('getLatest3ServiceOrders::', deferred.promise);
+                    return deferred.promise;
+                }
+            };
+
+
+            /**
+             * replace function of HomeService.getUsersObjectByName
+             */
+            this.getUsersObjectByName = function(str_name,isOnline) {
+                if(isOnline){
+                    let sql = "select Id,Name from user where Name like '%" + str_name + "%' limit 50"
+                    let url = service.buildURL('querySobjects',sql);
+                    let requestMethod = 'GET';
+                    return service.sendRest(url,requestMethod);
+                } else {
+                    console.log('getUsersObjectByName.Name:%s', str_name);
+                    let deferred = $q.defer();
+
+                    let sql = "select {User:_soup}\
+                         from {User}\
+                         where {User:Name} like '%" + str_name + "%'";
+                    let querySpec = navigator.smartstore.buildSmartQuerySpec(sql, SMARTSTORE_COMMON_SETTING.PAGE_SIZE_FOR_ALL);
+                    navigator.smartstore.runSmartQuery(querySpec, function (cursor) {
+                        let users = [];
+                        if (cursor && cursor.currentPageOrderedEntries && cursor.currentPageOrderedEntries.length) {
+                            angular.forEach(cursor.currentPageOrderedEntries, function (entry) {
+                                users.push({
+                                    Id: entry[0].Id,
+                                    Name: entry[0].Name,
+                                    _soupEntryId: entry[0]._soupEntryId
+                                });
+                            });
+                        }
+                        deferred.resolve(users);
+                    }, function (err) {
+                        $log.error(err);
+                        console.error(err);
+                        deferred.reject(err);
+                    });
+                    console.log('getUsersObjectByName::', deferred.promise);
+                    return deferred.promise;
+                }
+            };
+
+
+
+
+            //Util Methods Start.
             this.buildURL = function (str_type,str_param) {
                 return 'type=' + str_type + '&param=' + str_param;
             };
