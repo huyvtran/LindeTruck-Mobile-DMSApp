d40ebf2ba409c416629beb0a1dc17330845a4529
BoomGit <battlejacker@gmail.com>
2019-03-21 14:22:54 +0800

    修改shell脚本

--- tools/scripts/android/initialize.sh
+++ tools/scripts/android/initialize.sh
@@ -9,7 +9,7 @@ echo '\x1B[0;91mx PLEASE SET UP YOUR ANDROID_HOME PATH\x1B[0m'
    # exit
 # after setting up your ANDROID_HOME, please disable until here
 
-export ANDROID_HOME=/Users/gw/Library/Android/sdk >/dev/null 2>/dev/null
+export ANDROID_HOME=/Users/jiawei/Library/Android/sdk >/dev/null 2>/dev/null
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools >/dev/null 2>/dev/null
 
 # ANDROID_HOME
@@ -31,7 +31,7 @@ fi
 # plugin
 if [ -d plugins ]; then
     echo '\x1B[0;33m! "plugins"-folder already exists. Trying to delete.\x1B[0m'
-    rm -rf plugins >/dev/null 2>/dev/null
+    #rm -rf plugins >/dev/null 2>/dev/null
 else
     echo '\x1B[0;32m✓ No "plugins"-folder found.\x1B[0m'
 fi
@@ -150,11 +150,11 @@ cordova plugin add cordova-plugin-contacts
 echo '\x1B[0;90m✓ cordova-plugin-file-transfer.\x1B[0m'
 cordova plugin add cordova-plugin-file-transfer
 
-#echo '\x1B[0;90m✓ print-plugin.\x1B[0m'
-#cordova plugin add tools/plugin/print-plugin
+echo '\x1B[0;90m✓ print-plugin.\x1B[0m'
+cordova plugin add plugins/print-plugin
 #
-#echo '\x1B[0;90m✓ cordova-plugin-signature-view.\x1B[0m'
-#cordova plugin add tools/plugin/cordova-plugin-signature-view
+echo '\x1B[0;90m✓ cordova-plugin-signature-view.\x1B[0m'
+cordova plugin add plugins/cordova-plugin-signature-view
 
 echo '\x1B[0;90m✓ com.lampa.startapp.\x1B[0m'
 cordova plugin add https://github.com/lampaa/com.lampa.startapp.git
